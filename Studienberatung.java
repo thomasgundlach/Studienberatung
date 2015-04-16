9e91800fbaac190063a6025f319726df7dc1da49
Thomas Gundlach <Thomas.Gundlach@HAW-Hamburg.de>
2015-04-16 11:00:06 +0200

    initialized Scanner

---
+++ Studienberatung.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Studienberatung {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hast Du Abitur?");
+		String result = scanner.next();
+	}
+}
