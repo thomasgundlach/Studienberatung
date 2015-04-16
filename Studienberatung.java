afe8a764d899fea17a60c92c5e373148f54d9f21
Thomas Gundlach <Thomas.Gundlach@HAW-Hamburg.de>
2015-04-16 11:16:51 +0200

    added Question Abitur no

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,5 +5,11 @@ public class Studienberatung {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast Du Abitur?");
 		String result = scanner.next();
+		System.out.println("Hast du Abitur?");
+		String answer = scanner.next();
+		if (answer.equalsIgnoreCase("n") {
+			// hier Code für kein Abitur
+		} else {
+		}
 	}
 }
